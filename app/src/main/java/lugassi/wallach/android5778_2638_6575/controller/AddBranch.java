30b71e862896b71f86f6915936ffb46ca0346155
mlugassi <michael19191@gmail.com>
2017-11-28 23:24:12 +0200

    second

---
+++ app/src/main/java/lugassi/wallach/android5778_2638_6575/controller/AddBranch.java
@@ -0,0 +1,15 @@
+package lugassi.wallach.android5778_2638_6575.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lugassi.wallach.android5778_2638_6575.R;
+
+public class AddBranch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_branch);
+    }
+}
